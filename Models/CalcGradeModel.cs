4e4706bf5efc44d25b22e3eb0adae221cc6d7d2d
sagehassell <60795541+sagehassell@users.noreply.github.com>
2021-01-27 23:09:19 -0700

    Assignment Number 2 CHanges

---
+++ Models/CalcGradeModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SagesFirstASPNetWEbApp.Models
+{
+    public class CalcGradeModel
+    {
+        [Range(0,100,
+            ErrorMessage ="You must enter a number between 0 and 100")]
+        public float Assignments { get; set; }
+
+        [Range(0, 100,
+            ErrorMessage = "You must enter a number between 0 and 100")]
+        public float Gp { get; set; }
+
+        [Range(0, 100,
+            ErrorMessage = "You must enter a number between 0 and 100")]
+        public float Quiz { get; set; }
+
+        [Range(0, 100,
+            ErrorMessage = "You must enter a number between 0 and 100")]
+        public float Exam { get; set; }
+
+        [Range(0, 100,
+            ErrorMessage = "You must enter a number between 0 and 100")]
+        public float INTEX { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -16,14 +16,36 @@ namespace SagesFirstASPNetWEbApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsEnvironment("Development"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //Add a nice arror page
+            }
+            
+            app.UseNodeModules();
             //ordermatters for these 2
-            app.UseDefaultFiles(); //sends it straight to the index.html
+            //app.UseDefaultFiles(); //sends it straight to the index.html
             app.UseStaticFiles();  //allows us to use files in our subdirectories. 
+            
+            app.UseRouting(); //this means we no longer want users to have to type in the exact name of the page
+            
+            app.UseEndpoints(cfg =>
+            {
+                app.UseDeveloperExceptionPage();
+                cfg.MapControllerRoute("Default",
+                    "{controller}/{action}/{id?}",
+                    new { controller = "Home", action = "Index" });
+            }
+            );
         }
     }
 }
